d4e6f6559b3a48c59194321f0f9630057c066ca2
Justin Sorensen <justinsorensen83@gmail.com>
2014-01-22 17:24:28 -0800

    Quick test
    Sloppy page testing phoneGap cloud app creation.

---
+++ js/list.js
@@ -0,0 +1,6 @@
+function  makeList(){
+	for(i=0; i < 100; i++) {
+		document.getElementById("list").innerHTML += "<li> fig " + i + "</li>";
+	}
+	//document.getElementById("list").innerHTML = "<p> SUP YO!!!! </p>";
+}
